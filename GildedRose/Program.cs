b87a30ed94d545df1acdab8ee6bc96482d790650
Steven Mayer <stemayer@microsoft.com>
2018-09-10 16:17:26 -0700

    Angelo's first attempt

--- GildedRose/Program.cs
+++ GildedRose/Program.cs
@@ -8,47 +8,209 @@ namespace GildedRose
     class Program
     {
         public IList<Item> Items;
+
+        IList<Rule> Rules;
+        Rule DefaultRule;
+        Rule LegendaryRule;
         static void Main(string[] args)
         {
             System.Console.WriteLine("OMGHAI!");
 
-            var app = new Program()
+            var app = new Program();
+
+            app.UpdateQuality();
+
+            System.Console.ReadKey();
+        }
+
+        private void PopulateRules()
+        {
+            Rules = new List<Rule>
             {
-                Items = new List<Item>
-                {
-                    new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                    new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                    new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                    new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                    new Item
+                new AgedRule(),
+                new PassRule(),
+                new ConjuredRule()
+            };
+
+            DefaultRule = new DefaultRule();
+            LegendaryRule = new LegendaryRule();
+        }
+
+        private void PopulateItems()
+        {
+            Items = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item
                     {
                         Name = "Backstage passes to a TAFKAL80ETC concert",
                         SellIn = 15,
                         Quality = 20
                     },
-                    new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-                }
-
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
-
-            app.UpdateQuality();
-
-            System.Console.ReadKey();
-
         }
 
         public void UpdateQuality()
         {
-            for (var i = 0; i < Items.Count; i++)
+            //PopulateItems();
+            PopulateRules();
+
+            foreach (Item item in Items)
             {
-                UpdateQuality(Items[i]);
+                if (!LegendaryRule.CheckName(item))
+                {
+                    UpdateItemQualityWithRules(item);
+                    UpdateItemSellIn(item);
+                    if (item.SellIn < 0)
+                    {
+                        UpdateItemQualityWithRules(item);
+                    }
+                }
             }
         }
 
-        private void UpdateQuality(Item item)
+        private void UpdateItemSellIn(Item item)
         {
-            var product = ItemFactory.Create(item);
-            product.UpdateQuality();
+            item.SellIn = item.SellIn - 1;
+        }
+
+        private void UpdateItemQualityWithRules(Item item)
+        {
+            int newQuality = item.Quality;
+            bool qualityNotChanged = true;
+            foreach (Rule rule in Rules)
+            {
+                if (rule.CheckName(item))
+                {
+                    qualityNotChanged = false;
+                    newQuality += rule.QualityChange(item);
+                }
+            }
+            if (qualityNotChanged)
+            {
+                newQuality += DefaultRule.QualityChange(item);
+            }
+
+            item.Quality = CapQuality(newQuality);
+        }
+
+        private int CapQuality(int newQuality)
+        {
+            if (newQuality < 0)
+            {
+                newQuality = 0;
+            }
+            if (newQuality > 50)
+            {
+                newQuality = 50;
+            }
+            return newQuality;
+        }
+    }
+
+
+    /* Rule Definitions */
+
+    abstract class Rule
+    {
+        public virtual string NameFilter
+        {
+            get
+            {
+                return string.Empty;
+            }
+        }
+
+        public abstract int QualityChange(Item item);
+        public virtual bool CheckName(Item item)
+        {
+            string name = item.Name.ToLower();
+            return name.Contains(NameFilter.ToLower());
+        }
+    }
+
+    class DefaultRule : Rule
+    {
+        public override int QualityChange(Item item)
+        {
+            return -1;
+        }
+    }
+
+    class AgedRule : Rule
+    {
+        public override string NameFilter
+        {
+            get
+            {
+                return "aged";
+            }
+        }
+
+        public override int QualityChange(Item item)
+        {
+            return 1;
+        }
+    }
+
+    class ConjuredRule : Rule
+    {
+        public override string NameFilter
+        {
+            get
+            {
+                return "conjured";
+            }
+        }
+
+        public override int QualityChange(Item item)
+        {
+            return -2;
+        }
+    }
+
+    class LegendaryRule : Rule
+    {
+        public override string NameFilter
+        {
+            get
+            {
+                return "Sulfuras";
+            }
+        }
+
+        public override int QualityChange(Item item)
+        {
+            return 0;
+        }
+    }
+
+    class PassRule : Rule
+    {
+        public override string NameFilter
+        {
+            get
+            {
+                return "pass";
+            }
+        }
+
+        public override int QualityChange(Item item)
+        {
+            if (item.SellIn < 0)
+                return -item.Quality;
+
+            if (item.SellIn < 6)
+                return 3;
+
+            if (item.SellIn < 11)
+                return 2;
+
+            return 1;
         }
     }
 }
